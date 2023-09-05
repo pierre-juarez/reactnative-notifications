af0abea4796c938e5d58a78221cfac227f61908d
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-09-05 14:34:32 -0500

    [add]: Notification settings

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(remoteMessage => {
+  // Your code to handle notifications in killed state. For example
+  console.log('Killed state notification', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ usePushNotifications.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid, Platform} from 'react-native';
+import notifee from '@notifee/react-native';
+
+const usePushNotification = () => {
+  const requestUserPermission = async () => {
+    if (Platform.OS === 'ios') {
+      //Request iOS permission
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    } else if (Platform.OS === 'android') {
+      //Request Android permission (For API level 33+, for 32 or below is not required)
+      const res = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+    }
+  };
+
+  const getFCMToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log('Your Firebase Token is:', fcmToken);
+    } else {
+      console.log('Failed', 'No token received');
+    }
+  };
+
+  const listenToForegroundNotifications = async () => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log(
+        'A new message arrived! (FOREGROUND)',
+        JSON.stringify(remoteMessage),
+      );
+      console.log('antes de mostrar');
+      // Request permissions (required for iOS)
+      await notifee.requestPermission();
+
+      // Create a channel (required for Android)
+      const channelId = await notifee.createChannel({
+        id: 'sound',
+        name: 'Default Channel',
+      });
+
+      // Display a notification
+      await notifee.displayNotification({
+        title: remoteMessage.notification.title,
+        body: remoteMessage.notification.body,
+        android: {
+          channelId,
+        },
+      });
+      console.log('después de mostrar');
+      console.log(
+        '🚀 ~ file: usePushNotifications.js:44 ~ unsubscribe ~ remoteMessage.notification.title:',
+        remoteMessage.notification.title,
+      );
+    });
+    return unsubscribe;
+  };
+
+  const listenToBackgroundNotifications = async () => {
+    const unsubscribe = messaging().setBackgroundMessageHandler(
+      async remoteMessage => {
+        console.log(
+          'A new message arrived! (BACKGROUND)',
+          JSON.stringify(remoteMessage),
+        );
+      },
+    );
+    return unsubscribe;
+  };
+
+  const onNotificationOpenedAppFromBackground = async () => {
+    const unsubscribe = messaging().onNotificationOpenedApp(
+      async remoteMessage => {
+        console.log(
+          'App opened from BACKGROUND by tapping notification:',
+          JSON.stringify(remoteMessage),
+        );
+      },
+    );
+    return unsubscribe;
+  };
+
+  const onNotificationOpenedAppFromQuit = async () => {
+    const message = await messaging().getInitialNotification();
+
+    if (message) {
+      console.log(
+        'App opened from QUIT by tapping notification:',
+        JSON.stringify(message),
+      );
+    }
+  };
+
+  return {
+    requestUserPermission,
+    getFCMToken,
+    listenToForegroundNotifications,
+    listenToBackgroundNotifications,
+    onNotificationOpenedAppFromBackground,
+    onNotificationOpenedAppFromQuit,
+  };
+};
+
+export default usePushNotification;
